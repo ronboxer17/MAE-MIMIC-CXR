9996ed5716eb33f11b1f9d66ddeb5eec71742a98
freddd1 <itamar.fradkin@gmail.com>
2022-12-24 13:31:46 +0200

    renamed and added paths

---
+++ src/config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import os
+
+ROOT_DIR = Path(__file__).parent.parent  # This is your Project Root
+
+METADATA_PATH = os.path.join(ROOT_DIR, 'data/mimic-cxr/mimic-cxr-2.0.0-metadata.csv')
+SPLIT_PATH = os.path.join(ROOT_DIR, 'data/mimic-cxr/mimic-cxr-2.0.0-split.csv')
+CHEXPERT_PATH = os.path.join(ROOT_DIR, 'data/chexpert/mimic-cxr-2.0.0-chexpert.csv')
+NEGIBOX_PATH = os.path.join(ROOT_DIR, 'data/negibox/mimic-cxr-2.0.0-negibox.csv')
+
--- src/utils.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-
-ROOT_DIR = sys.path[1]  # This is your Project Root
