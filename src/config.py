5fdfd64917fcb7b817f56b010513107e79cb5659
freddd1 <itamar.fradkin@gmail.com>
2022-12-31 18:08:09 +0200

    fix naming

--- src/config.py
+++ src/config.py
@@ -5,6 +5,6 @@ ROOT_DIR = Path(__file__).parent.parent  # This is your Project Root
 
 METADATA_PATH = os.path.join(ROOT_DIR, 'data/mimic-cxr/mimic-cxr-2.0.0-metadata.csv')
 SPLIT_PATH = os.path.join(ROOT_DIR, 'data/mimic-cxr/mimic-cxr-2.0.0-split.csv')
-CHEXPERT_PATH = os.path.join(ROOT_DIR, 'data/chexpert/mimic-cxr-2.0.0-chexpert.csv')
-NEGIBOX_PATH = os.path.join(ROOT_DIR, 'data/negibox/mimic-cxr-2.0.0-negibox.csv')
+CHEXPERT_PATH = os.path.join(ROOT_DIR, 'data/mimic-cxr/mimic-cxr-2.0.0-chexpert.csv')
+NEGIBOX_PATH = os.path.join(ROOT_DIR, 'data/mimic-cxr/mimic-cxr-2.0.0-negbio.csv')
 
